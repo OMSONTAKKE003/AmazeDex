a6b3f74259110e287bb369c6e4a37d0740963f7d
LUV JAIN <luvjain22307@gmail.com>
2026-07-06 11:11:18 +0530

    Updated collison geometry, camera angles, tile colors

--- AmazeDex1/my-robot/mj_mink_right.py
+++ AmazeDex1/my-robot/mj_mink_right.py
@@ -4,12 +4,25 @@ import mujoco.viewer
 ROOT_PATH = "C:\\Users\\luvja\\Desktop\\updatedwithstand\\AmazeDex1"
 
 model = mujoco.MjModel.from_xml_path(
-            f"{ROOT_PATH}\\my-robot\\scene.xml"
-        )
-#MODEL_PATH = "C:\\Users\\luvja\\Desktop\\amaze\\AmazingHand-main\\Demo\\AHSimulation\\AHSimulation\\Hand_Demo\\AH_Right\\mjcf\\scene.xml"
-#model = mujoco.MjModel.from_xml_path(model)
+    f"{ROOT_PATH}\\my-robot\\scene.xml"
+)
 data = mujoco.MjData(model)
 
-with mujoco.viewer.launch(model, data):
-    while True:
+# Use launch_passive to gain control over the viewer object and your simulation loop
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    
+    # Simulation loop
+    while viewer.is_running():
         mujoco.mj_step(model, data)
+        with viewer.lock():
+            # Set the camera to a fixed position and orientation
+         viewer.cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
+         viewer.cam.trackbodyid = 1
+         viewer.cam.azimuth = 55
+         viewer.cam.elevation = -50
+         viewer.cam.distance = 0.4
+         viewer.cam.lookat[:] = [0.3 , 0.4, 0.5]
+
+        
+        # Pick up changes and refresh the viewer frame
+        viewer.sync()
