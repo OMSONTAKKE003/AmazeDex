8b1bc3acbe8763b90b80fbc3ab929fe17e9dbe97
LUV JAIN <luvjain22307@gmail.com>
2026-07-07 09:06:58 +0530

    RL Practice and Environment Corrections

--- AmazeDex1/my-robot/mj_mink_right.py
+++ AmazeDex1/my-robot/mj_mink_right.py
@@ -1,5 +1,7 @@
 import mujoco
 import mujoco.viewer
+import time 
+import numpy as np
 
 ROOT_PATH = "C:\\Users\\luvja\\Desktop\\updatedwithstand\\AmazeDex1"
 
@@ -14,6 +16,16 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
     # Simulation loop
     while viewer.is_running():
         mujoco.mj_step(model, data)
+        t = data.time
+        data.ctrl[0] = np.sin(t+1 )  #thumb
+        data.ctrl[1] = np.sin(t+3 )  #thumb 
+        data.ctrl[2] = np.cos(t +2)   #finger 1
+        data.ctrl[3] = np.sin(3*t+3)  #finger 1
+        data.ctrl[4] = np.cos(t+5 ) #finger 2
+        data.ctrl[5] = np.cos(5*t +2) #finger 2
+        data.ctrl[6] = np.tan(2*t+1) #finger 3
+        data.ctrl[7] = np.cos(t+0.5 ) #finger 3
+
         with viewer.lock():
             # Set the camera to a fixed position and orientation
          viewer.cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
